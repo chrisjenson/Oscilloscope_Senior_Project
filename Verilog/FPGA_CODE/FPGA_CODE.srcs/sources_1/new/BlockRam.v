2304c64e9ad8d4dc07b96ee8fd4fd727427ef0c9
brianworts <wortsb1@tcnj.edu>
2020-10-04 12:59:42 -0400

    Adding additional files

--- Verilog/FPGA_CODE/FPGA_CODE.srcs/sources_1/new/BlockRam.v
+++ Verilog/FPGA_CODE/FPGA_CODE.srcs/sources_1/new/BlockRam.v
@@ -1,25 +1,34 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/19/2020 06:49:29 PM
-// Design Name: 
-// Module Name: BlockRam
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
+module ram_model_1w1r
+#( parameter
+    WORDLENGTH = 32,
+    LOG2_DEPTH =  2
+)
+(
+    input      [LOG2_DEPTH-1:0] IN_WADR,
+    input                       IN_WEN,
+    input      [WORDLENGTH-1:0] IN_WDAT,
+    
+    input      [LOG2_DEPTH-1:0] OUT_RADR,
+    output     [WORDLENGTH-1:0] OUT_RDAT,
+    
+    input                       clk
+);
+
+reg [WORDLENGTH-1:0]        dbuf[(1<<LOG2_DEPTH)-1:0];    // storage array
+
+assign OUT_RDAT = dbuf[OUT_RADR];      // retrieve read data
+
+// num_in_buf
+always @ (posedge clk)
+begin
+    if (IN_WEN)
+        dbuf[IN_WADR]   <= IN_WDAT;             // store write data
+end
+
+/*
 module BlockRam(
     input [7:0] writeData, readData,
 	input [18:0] writeAddr, readAddr,
@@ -64,3 +73,4 @@ module BlockRam(
 	end
     
 endmodule
+*/
