aeb6078a5f93e01826b3ff44e5db3b4725a9f645
brianworts <wortsb1@tcnj.edu>
2020-11-01 14:11:06 -0500

    updates to SPI

--- Verilog/SPI_Test/SPI_Test.srcs/sim_1/new/tb.v
+++ Verilog/SPI_Test/SPI_Test.srcs/sim_1/new/tb.v
@@ -26,10 +26,19 @@ module tb();
     reg SCLK;
     reg [1:0] SCLKCount;
     
+    //reg MISO;
+    reg MOSI;
+    reg SlaveSel;
+
+    
+    
     initial
     begin
         clk = 0;
+        SCLK = 0;
         SCLKCount = 0;
+        MOSI = 0;
+        SlaveSel =1;
         while (1)
             #5 clk = ~clk;  // toggle clk each 5 ns (100 MHz clock frequency)
     end
@@ -42,5 +51,37 @@ module tb();
             SCLK = ~SCLK;
         end
     end
+   
+    reg [15:0] SPIWord;
+    reg [3:0] MOSICount;
+    always @(posedge SCLK)
+    begin
+        if(!SlaveSel)
+        begin
+            MOSI <= SPIWord[MOSICount];
+            MOSICount <= MOSICount + 1;
+        end
+        else
+        begin
+            MOSICount <=0;
+        end
+    end
+    
+    
+    initial begin
+        SPIWord = 16'b0010000100000000;
+        #100 SlaveSel = 0;
+    end
+    
+    
+    
+    
+    SPI u_SPI(
+        .MISO(),
+        .MOSI(MOSI),
+        .SlaveSel(SlaveSel),
+        .clk(clk),
+        .SCLK(SCLK) //DEBUG Generate Slave Clock???
+    );
     
 endmodule
--- Verilog/SPI_Test/SPI_Test.srcs/sources_1/new/SPI.v
+++ Verilog/SPI_Test/SPI_Test.srcs/sources_1/new/SPI.v
@@ -2,10 +2,10 @@
 
 module SPI(
         output reg MISO,
-        input MOSI,
+        input MOSI_Raw,
         input SlaveSel,
         input clk,
-        input SCLK //DEBUG Generate Slave Clock???
+        input SCLK_Raw
     );
     
     reg Reg_WrEn;
@@ -23,72 +23,99 @@ module SPI(
     assign SPI_Cmd = SPIWord[15:13];
     assign SPI_Params = SPIWord[12:8];
     assign SPI_Data = SPIWord[7:0];
-    
+        
     reg [7:0] Reg_DataOut;
     reg [7:0] Reg_DataIn;
+    
+    reg sck;
+    reg sck_p1;
+    reg sck_raw_p1;
+    reg MOSI_raw_p1;
+    reg MOSI;
+    reg sck_posedge_pulse;
      
     always @(*)
     begin
         case (SPI_Cmd)
-            3'b000: //ReadReg- MISO //DEBUG THIS MAY CAUSE ISSUES SINCE DEFAULT IS 000
+            3'b001: //ReadReg- MISO //DEBUG 000 MAY CAUSE ISSUES SINCE DEFAULT IS 000
             begin
                 Reg_RdEn = 1;
                 Reg_WrEn = 0;
             end
-            3'b001: //Write- MOSI
+            3'b010: //Write- MOSI
             begin
                 Reg_RdEn = 0;
                 Reg_WrEn = 1;
             end
-            3'b010: //ReadData- MISO
+            3'b011: //ReadData- MISO RAM Read
             begin
                 Reg_RdEn = 0;
                 Reg_WrEn = 0;
             end
         endcase
     end
+    
     always @(posedge clk)
     begin
+        sck_p1 <= sck; //one cycle delay
+        
+        sck_raw_p1 <= SCLK_Raw;
+        sck <= sck_raw_p1;
+        
+        MOSI_raw_p1 <= MOSI_Raw;
+        MOSI <= MOSI_raw_p1;
+    end
+    assign sck_posedge_pulse = sck & ~sck_p1;
+    
+    always @(posedge clk)       
+    begin
+     
         if (SlaveSel) //Do not read on high
         begin
             SPI_InSixteenCount <= 0;
-            SPI_OutSixteenCount <= 0;
+            SPI_OutSixteenCount <= 4'hF;
             SPI_DataIn <= 16'h0000;
             SPIWord <= 16'h0000; //DEBUG This will trigger cmd=000
             
             Reg_RdEn <= 0;//DEBUG should these go here?
             Reg_WrEn <= 0;
+            
+            
         end
-    end
-    always @(posedge SCLK)//DEBUG okay to use SCLK???
-    begin
-        if (!SlaveSel)
+    //end
+    //always @(posedge SCLK)//DEBUG okay to use SCLK???
+    //begin
+        if (sck_posedge_pulse)
         begin
-            //Read 1 bit at a time
-            if (!Reg_RdEn)
+            if (!SlaveSel)
             begin
-                SPI_DataIn[SPI_InSixteenCount] <= MOSI;
-                if (SPI_InSixteenCount == 4'hF)
+                //Read 1 bit at a time
+                if (!Reg_RdEn)
                 begin
-                    SPIWord <= SPI_DataIn;
-                end
-                
-                SPI_InSixteenCount <= SPI_InSixteenCount + 1;
-            end
-           
-            if (Reg_RdEn)
-            begin
-                MISO <= Reg_DataOut[SPI_OutSixteenCount];
-                if (SPI_OutSixteenCount == 4'hF)
-                begin
+                    SPI_DataIn[SPI_InSixteenCount] <= MOSI;
+                    if (SPI_InSixteenCount == 4'h0)
+                    begin
+                        SPIWord <= {SPI_DataIn[15:1], MOSI}; //Valid word including 0th bit
+                    end
                     
+                    SPI_InSixteenCount <= SPI_InSixteenCount - 1;
                 end
-                SPI_OutSixteenCount <= SPI_OutSixteenCount +1;
+                //Drive on negedge and sample on pos edge so need opposite of negative edge here
+                //move this outside of if(posedge), maybe into new always @posedge clk
+                if (Reg_RdEn)
+                begin
+                    MISO <= Reg_DataOut[SPI_OutSixteenCount];
+                    if (SPI_OutSixteenCount == 4'hF) //MSB first and not LSB
+                    begin
+                        //DEBUG WE ARE HERE
+                    end
+                    SPI_OutSixteenCount <= SPI_OutSixteenCount +1;
+                end
             end
         end
     end
      
-    Regs(
+    Regs u_REGS(
         .Write_Data(Reg_DataIn),
         .Write_Addr(SPI_Params),
         .WrEn(Reg_WrEn),
