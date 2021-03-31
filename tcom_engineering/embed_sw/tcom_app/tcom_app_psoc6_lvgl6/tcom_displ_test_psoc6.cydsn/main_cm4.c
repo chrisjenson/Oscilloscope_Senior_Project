0e0938272c9606e70c108b7dcb13812def96ee9f
Chris <chris.jenson10@gmail.com>
2021-03-31 12:52:53 -0400

    SPI Cleanup

--- tcom_engineering/embed_sw/tcom_app/tcom_app_psoc6_lvgl6/tcom_displ_test_psoc6.cydsn/main_cm4.c
+++ tcom_engineering/embed_sw/tcom_app/tcom_app_psoc6_lvgl6/tcom_displ_test_psoc6.cydsn/main_cm4.c
@@ -103,102 +103,75 @@ main( void )
     cm4.onBit = 0b00000000;
     cm4.windowPos = 1024;
     
+  
     
-    //if(cm4.onBit == 0b00000001)
-    //{
-        //construct write commmand for updating offset in register 11
-        cm4.TxBuffer[0] = 0b0100101100000000 | cm4.Offset;     //010 01011 00000000
-        
-        //construct write commmand for updating trigger in register 4
-        cm4.TxBuffer[1] = 0b0100010000000000 | cm4.Trigger;     //010 00100 00000000
-        
-        //construct write commmand for updating gain in register 14
-        cm4.TxBuffer[2] = 0b0100111000000000 | cm4.Gain;     //010 01110 00000000
-        
-        //construct read RAM command, or with HoriScale to determine the sample size
-        //cm4.TxBuffer[3] = 0b0110000000000000 | cm4.HoriScale;     //011 00000 00000000
-        
-        //construct write commmand for updating on-bit in register 9
-        cm4.TxBuffer[2] = 0b0100100100000000;     //010 01001 00000000
-        
-        //Send a read ram command to read 1024x2 samples 
-        //cm4.TxBuffer[0] = 0b011000000000000; //| cm4.HoriScale;     //011 01001 00000000
-        cm4.TxBuffer[0] = 0b0110100000000000;  //011 01001 00000000
-        
-        
-        //write reg 4 all zeroes
-        //cm4.TxBuffer[0] = 0b0100010000000000;  //011 01001 00000000
-        
-        CyDelay(3000);
-        
-        uint32_t NUM_TO_WRITE = 8; 
-        int ss_state = 1;
-        uint16_t bufferIndex = 0; 
-        
-        //loop to send each command in the txBuffer
-        for(int i = 0; i < 1; ++i){
-            //Set slave select low for the commands
-            ss_state = !ss_state;  Cy_GPIO_Write(SEL_PIN_PORT, SEL_PIN_NUM, ss_state);
+    //------------------START SPI CODE---------------------------------
+    //construct write commmand for updating trigger in register 4
+    //cm4.TxBuffer[0] = 0b0100010000000000 | cm4.Trigger;     //010 00100 00000000
+    cm4.TxBuffer[0] = 0b0100010000000000;     //010 00100 00000000
+    
+    //construct write commmand for updating on-bit in register 9
+    //cm4.TxBuffer[1] = 0b0100100100000000 | cm4.onBit;     //010 01001 00000000
+    cm4.TxBuffer[1] = 0b0100100100000000;     //010 01001 00000000
+    
+    //construct read commmand for checking a trigger event in register 15
+    cm4.TxBuffer[2] = 0b0010111100000000;     //001 01111 00000000
+    
+    //Send a read ram command to read 1024x2 samples 
+    cm4.TxBuffer[3] = 0b0110100000000000;  //011 01001 00000000
+    
+    uint32_t NUM_TO_WRITE = 8; 
+    int ss_state = 1;
+    uint16_t bufferIndex = 0; 
+    
+    //loop to send each command in the txBuffer
+    for(int i = 3; i < 4; ++i){
+        //Set slave select low for the commands
+        ss_state = !ss_state;  Cy_GPIO_Write(SEL_PIN_PORT, SEL_PIN_NUM, ss_state);
 
+        Cy_SCB_SPI_ClearSlaveMasterStatus(SPIM_HW, CY_SCB_SPI_MASTER_DONE);
+        Cy_SCB_SPI_Write(SPIM_HW, cm4.TxBuffer[i]);
+        while(!(Cy_SCB_SPI_GetTxFifoStatus(SPIM_HW) & CY_SCB_SPI_TX_EMPTY)){}
+        while(!(Cy_SCB_SPI_GetSlaveMasterStatus(SPIM_HW) & CY_SCB_SPI_MASTER_DONE)){}
+
+        // Clear out the RX FIFO
+        while((Cy_SCB_SPI_GetNumInRxFifo(SPIM_HW) != 1)){}
+        Cy_SCB_SPI_ReadArray(SPIM_HW, cm4.RxBuffer, 1);
+
+        //1024/NUMBER OF BURSTS, total number of samples divided by the size of bursts
+        //1024 because we are reading in  2 samples every time
+        for(uint32_t burst = 0; burst < 1024/NUM_TO_WRITE; burst++){
+
+            // Dummy write, to read data from FPGA
             Cy_SCB_SPI_ClearSlaveMasterStatus(SPIM_HW, CY_SCB_SPI_MASTER_DONE);
-            Cy_SCB_SPI_Write(SPIM_HW, cm4.TxBuffer[i]);
+            Cy_SCB_SPI_WriteArray(SPIM_HW, cm4.TxBuffer, NUM_TO_WRITE);
             while(!(Cy_SCB_SPI_GetTxFifoStatus(SPIM_HW) & CY_SCB_SPI_TX_EMPTY)){}
             while(!(Cy_SCB_SPI_GetSlaveMasterStatus(SPIM_HW) & CY_SCB_SPI_MASTER_DONE)){}
-
-            // Clear out the RX FIFO
-            while((Cy_SCB_SPI_GetNumInRxFifo(SPIM_HW) != 1)){}
-            Cy_SCB_SPI_ReadArray(SPIM_HW, cm4.RxBuffer, 1);
-
-            /*
-            These two lines work for reading the registers
-            if((cm4.TxBuffer[i] >> 13) == 0b0000000000000001 || (cm4.TxBuffer[i] >> 13) == 0b0000000000000010){
-               while(!(Cy_SCB_SPI_GetRxFifoStatus(SPIM_HW) & CY_SCB_SPI_RX_NOT_EMPTY)){}
-                cm4.RxBuffer[0] = Cy_SCB_SPI_Read(SPIM_HW);
-            }
-            */
             
-            //else{
-            //1024/8, total number of samples divided by the size of bursts
-            //512 because we are reading in  2 samples every time
-            for(uint32_t burst = 0; burst < 1024/NUM_TO_WRITE; burst++){
-
-                // Dummy write, to read data from FPGA
-                Cy_SCB_SPI_ClearSlaveMasterStatus(SPIM_HW, CY_SCB_SPI_MASTER_DONE);
-                Cy_SCB_SPI_WriteArray(SPIM_HW, cm4.TxBuffer, NUM_TO_WRITE);
-                while(!(Cy_SCB_SPI_GetTxFifoStatus(SPIM_HW) & CY_SCB_SPI_TX_EMPTY)){}
-                while(!(Cy_SCB_SPI_GetSlaveMasterStatus(SPIM_HW) & CY_SCB_SPI_MASTER_DONE)){}
-                
-                //read in bursts of NUM_TO_WRITE size
-                while((Cy_SCB_SPI_GetNumInRxFifo(SPIM_HW) != NUM_TO_WRITE)){}
-                Cy_SCB_SPI_ReadArray(SPIM_HW, cm4.RxBuffer, NUM_TO_WRITE);
-                
-                //Check if a ram read was performed so we can check if data has to be parsed
-                if((cm4.TxBuffer[i] >> 13) == 0b011){
-                    //for loop to split the read in data and then store appropriately in cm4.RamReadBuffer
-                    //NUM_TO_WRITE*2 because we are actually reading in 2 data points per buffer space
-                    for(uint16_t count = 0; count < NUM_TO_WRITE; count++){
-                        //spilt the data, dont need variables here but are helpful
-                        uint8_t firstSample = cm4.RxBuffer[count] >> 8; //right shift 8 to remove the second value
-                        uint8_t secondSample = cm4.RxBuffer[count] & 0b0000000011111111; //AND with 1's to only get the first 8 digits
-                        
-                        cm4.RamReadBuffer[bufferIndex] = firstSample;
-                        cm4.RamReadBuffer[bufferIndex + 1] = secondSample;
-                        
-                        bufferIndex += 2;
-                    }
-                }
-                else{
-                    //carry on as usual.... however that may be
+            //read in bursts of NUM_TO_WRITE size
+            while((Cy_SCB_SPI_GetNumInRxFifo(SPIM_HW) != NUM_TO_WRITE)){}
+            Cy_SCB_SPI_ReadArray(SPIM_HW, cm4.RxBuffer, NUM_TO_WRITE);
+            
+            //Check if a ram read was performed so we can check if data has to be parsed
+            if((cm4.TxBuffer[i] >> 13) == 0b011){
+                //for loop to split the read in data and then store appropriately in cm4.RamReadBuffer
+                //NUM_TO_WRITE*2 because we are actually reading in 2 data points per buffer space
+                for(uint16_t count = 0; count < NUM_TO_WRITE; count++){
+                    //spilt the data, dont need variables here but are helpful
+                    uint8_t firstSample = cm4.RxBuffer[count] >> 8; //right shift 8 to remove the second value
+                    uint8_t secondSample = cm4.RxBuffer[count] & 0b0000000011111111; //AND with 1's to only get the first 8 digits
+                    
+                    cm4.RamReadBuffer[bufferIndex] = firstSample;
+                    cm4.RamReadBuffer[bufferIndex + 1] = secondSample;
+                    
+                    bufferIndex += 2;
                 }
             }
-            //Reassert slave select
-            Cy_GPIO_Write(SEL_PIN_PORT, SEL_PIN_NUM, 1);
         }
-        
-        //Reset to avoid an infinite loop
-        cm4.onBit = 0b00000000;
-    //}
-    // END SPI CODE //
+        //Reassert slave select
+        Cy_GPIO_Write(SEL_PIN_PORT, SEL_PIN_NUM, 1);
+    }
+    //-------------------END SPI CODE---------------------------------------------//
     
     
         
@@ -207,12 +180,11 @@ main( void )
         
        
     
-     home_screen();
+    home_screen();
     
     while (1)
     {
         lv_task_handler();
-        //btn_anime();
         
         CyDelay(5);
     }
